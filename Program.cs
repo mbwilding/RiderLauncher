35f7b9d652c2e07d72ffb962d8081a7bb8c1d6bd
mbwilding <mbwilding@gmail.com>
2022-03-26 18:42:12 +0800

    Init

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+StartApplication(FindExe());
+
+string FindExe()
+{
+    string localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+    string riderPre = @"JetBrains\Toolbox\apps\Rider\ch-0";
+    string riderPreDir = Path.Combine(localAppData, riderPre);
+    List<string> dirs = Directory.GetDirectories(riderPreDir)
+        .Where(x => !x.Contains("plugins"))
+        .ToList();
+    string? latestVersion = dirs.Select(x=> new Version(Path.GetFileName(x))).Max()?.ToString();
+    if (latestVersion is null)
+        Environment.Exit(1);
+    return Path.Combine(riderPreDir, latestVersion, @"bin\rider64.exe");
+}
+
+void StartApplication(string filePath)
+{
+    Process.Start(new ProcessStartInfo
+    {
+        FileName = filePath,
+        Arguments = args.FirstOrDefault(),
+        UseShellExecute = false
+    });
+}
